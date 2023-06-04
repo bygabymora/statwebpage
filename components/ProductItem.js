4e32f3a73c6c6d96ea99c82ad5a9db87c751d3ce
bygabymora <bygabymora@gmail.com>
2023-06-04 15:40:05 -0500

    paths

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -28,7 +28,7 @@ export const ProductItem = ({ product }) => {
     <div className="card">
       <Link href={{ pathname: `/products/${product.slug}` }}>
         <Image
-          src={`/products/${product.image}`}
+          src={`products/${product.image}`}
           alt={product.description}
           className="rounded shadow product-image"
           width={300}
--- utils/data.js
+++ utils/data.js
@@ -3,7 +3,7 @@ const data = {
     {
       manufacturer: 'MEDTRONIC',
       slug: 'MEDTRONIC7510400',
-      image: './images/7510400-WE1.png',
+      image: '/images/7510400-WE1.png',
       reference: '7510400',
       description: 'Bone Graft Kit, Medium Size',
       price: 4240,
@@ -28,7 +28,7 @@ const data = {
     {
       manufacturer: 'MEDTRONIC',
       slug: 'MEDTRONIC7510600',
-      image: './images/7510600-WE1.png',
+      image: '/images/7510600-WE1.png',
       reference: '7510600',
       description: 'Bone Graft Kit, Large Size',
       price: 4500,
@@ -41,7 +41,7 @@ const data = {
     {
       manufacturer: 'COVIDIEN ENERGY',
       slug: 'COVIDIENENERGYLF1837',
-      image: './images/LF1837-WE1.png',
+      image: '/images/LF1837-WE1.png',
       reference: 'LF1837',
       description:
         'Sealer/Divider, 5 mm dia. x 37 cm Shaft Length, 17.8 mm Cut Length, 19.5 mm Seal Length, Blunt, Double-action Jaw Style, 180° Shaft Rotation, Sterile, Single-Use, For Compatible with ForceTriad SW v3.6 or Higher',
@@ -54,7 +54,7 @@ const data = {
     {
       manufacturer: 'STRYKER',
       slug: 'STRYKER5820-107-530C',
-      image: './images/5820-107-530C-WE1.png',
+      image: '/images/5820-107-530C-WE1.png',
       reference: '5820-107-530C',
       description: 'Head Drill, 3 x 3.8 mm, Neuro Carbide',
       price: 75,
