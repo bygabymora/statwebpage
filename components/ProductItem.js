744196a3568fb65c88fb0ede7a59fc18f5f60656
bygabymora <bygabymora@gmail.com>
2023-06-04 17:02:56 -0500

    insert1

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -28,7 +28,7 @@ export const ProductItem = ({ product }) => {
     <div className="card">
       <Link href={{ pathname: `products/${product.slug}` }}>
         <Image
-          src={`products/images/${product.image}`}
+          src={`pages/products/images/${product.image}`}
           alt={product.description}
           className="rounded shadow product-image"
           width={300}
