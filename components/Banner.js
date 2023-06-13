55b5c61233d06526f3924ba1aa3b6dbd5d1f044d
bygabymora <bygabymora@gmail.com>
2023-06-12 20:26:03 -0500

    buttons

--- components/Banner.js
+++ components/Banner.js
@@ -27,7 +27,7 @@ const Banner = () => {
 
   return (
     <div className="text-title-color-dark text-center ">
-      <div className="grid grid-cols-2 sm:grid-cols-1 banner-container mx-auto px-4 py-8 items-center">
+      <div className="grid lg:grid-cols-2 md:grid-cols-1 banner-container mx-auto px-4 py-8 items-center">
         <div className="">
           <h1 className="text-4xl font-bold mb-4">
             Welcome to STAT Surgical Supplies
@@ -53,17 +53,17 @@ const Banner = () => {
             )}
           </p>
           {audience !== '' && (
-            <div className="mt-8 w-full mb-8 text-center">
+            <div className="grid grid-cols-2 mt-8 w-full mb-8 text-center">
               <button
                 href="tel:+11234567890"
                 onClick={handleCallButtonClick}
-                className="btn-call sm:inline-block block mr-4 mb-4 sm:mb-0 text-white bg-title-color-dark hover:bg-title-color px-6 py-3 rounded"
+                className="equal-button-size btn-call sm:inline-block block mr-4 mb-4 sm:mb-0 text-white bg-title-color-dark hover:bg-title-color px-6 py-3 rounded"
               >
                 Call Now
               </button>
               <button
                 href="#contact"
-                className="btn-contact sm:inline-block block text-title-color-dark hover:text-title-color px-6 py-3 rounded"
+                className=" equal-button-size btn-contact sm:inline-block block text-title-color-dark hover:text-title-color px-6 py-3 rounded"
               >
                 Contact Us
               </button>
