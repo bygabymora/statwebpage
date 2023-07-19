96186b964c5d9f73d71c5a5952048064b5d38997
bygabymora <bygabymora@gmail.com>
2023-07-18 22:13:26 -0500

    commited

--- components/StaticBanner.js
+++ components/StaticBanner.js
@@ -6,17 +6,18 @@ const StaticBanner = () => {
   return (
     <div className="static-banner-container mt-4">
       <div className="banner-content">
-        <h2 className="banner-title">Join Our Network of Manufacturers</h2>
+        <h2 className="banner-title">
+          Partner With Us and Sell Your Surgical Disposables
+        </h2>
         <p className="banner-description">
-          Partner with us to deliver safe, ethical, and high-quality products to
-          hospitals and surgery centers. Maximize your reach and increase your
-          revenue by working with STAT Surgical Supplies.
+          Stat Surgical Supply is looking to fill our customer demand. We pay
+          top dollar for in-date high-end surgical disposables.
         </p>
         <Link
           className="flex justify-center items-center"
           href="/ManufacturerForm"
         >
-          <span className="banner-link">Send us your product listing</span>
+          <span className="banner-link">Send us your list</span>
           <span className="link-space">&nbsp;&nbsp;</span>
           <AiOutlineSend className="link-space" />
         </Link>
