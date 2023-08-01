33f462f27272b093f4151bb22c253c643fd47d98
bygabymora <bygabymora@gmail.com>
2023-08-01 17:34:23 -0500

    user

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth/next';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import db from '../../../utils/db';
-import User from '../../../models/User';
+import User from '../../../models/user';
 import bcryptjs from 'bcryptjs';
 
 export default NextAuth({
--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,4 +1,4 @@
-import User from '../../models/User';
+import User from '../../models/user';
 import db from '../../utils/db';
 import data from '../../utils/data';
 import Product from '../../models/Product';
