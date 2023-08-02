f9ec407505666e044203d8be68d4ee4f55767277
bygabymora <bygabymora@gmail.com>
2023-08-02 17:55:33 -0500

    Order ID shorter

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -199,7 +199,10 @@ function OrderScreen() {
 
   return (
     <Layout title={`Order ${orderId}`}>
-      <h1 className="mb-4 text-xl">{`Order ${orderId}`}</h1>
+      <h1 className="mb-4 text-xl">{`Order ${orderId
+        .substring(orderId.length - 8)
+        .toUpperCase()}`}</h1>
+
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
