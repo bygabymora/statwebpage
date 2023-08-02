c593994d8c4bcae5a5524b93ec32164d13d0e1dd
bygabymora <bygabymora@gmail.com>
2023-08-02 17:48:23 -0500

    admin users functional

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -1,11 +1,80 @@
-import React from 'react';
-import Layout from '../../components/Layout';
+import axios from 'axios';
 import Link from 'next/link';
+import React, { useEffect, useReducer } from 'react';
+import { toast } from 'react-toastify';
+import Layout from '../../components/Layout';
+import { getError } from '../../utils/error';
+import { BiSolidEdit } from 'react-icons/bi';
+import { useRouter } from 'next/router';
+import { BsTrash3 } from 'react-icons/bs';
 
-export default function users() {
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, users: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    case 'DELETE_REQUEST':
+      return { ...state, loadingDelete: true };
+    case 'DELETE_SUCCESS':
+      return { ...state, loadingDelete: false, successDelete: true };
+    case 'DELETE_FAIL':
+      return { ...state, loadingDelete: false };
+    case 'DELETE_RESET':
+      return { ...state, loadingDelete: false, successDelete: false };
+    default:
+      return state;
+  }
+}
+
+function AdminUsersScreen() {
+  const router = useRouter();
+
+  const [{ loading, error, users, successDelete, loadingDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      users: [],
+      error: '',
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/users`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
+
+  const deleteHandler = async (userId) => {
+    if (!window.confirm('Are you sure?')) {
+      return;
+    }
+    try {
+      dispatch({ type: 'DELETE_REQUEST' });
+      await axios.delete(`/api/admin/users/${userId}`);
+      dispatch({ type: 'DELETE_SUCCESS' });
+      toast.success('User deleted successfully');
+    } catch (err) {
+      dispatch({ type: 'DELETE_FAIL' });
+      toast.error(getError(err));
+    }
+  };
+
   return (
-    <Layout>
-      <div className="grid md:grid-cols-4 md:gap-2">
+    <Layout title="Users">
+      <div className="grid md:grid-cols-4 md:gap-5">
         <div>
           <ul>
             <li>
@@ -24,8 +93,60 @@ export default function users() {
             </li>
           </ul>
         </div>
-        <h1 className="mb-4 text-xl">Admin Users</h1>
+        <div className="overflow-x-auto md:col-span-3">
+          <h1 className="mb-4 text-xl">Users</h1>
+          {loadingDelete && <div>Deleting...</div>}
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">ID</th>
+                    <th className="p-5 text-left">NAME</th>
+                    <th className="p-5 text-left">EMAIL</th>
+                    <th className="p-5 text-left">ADMIN</th>
+                    <th className="p-5 text-left">ACTIONS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user) => (
+                    <tr key={user._id} className="border-b">
+                      <td className=" p-5 ">{user._id.substring(20, 24)}</td>
+                      <td className=" p-5 ">{user.name}</td>
+                      <td className=" p-5 ">{user.email}</td>
+                      <td className=" p-5 ">{user.isAdmin ? 'YES' : 'NO'}</td>
+                      <td className=" p-5 ">
+                        <button
+                          onClick={() => router.push(`/admin/user/${user._id}`)}
+                          type="button"
+                          className="primary-button font-bold underline mr-2 "
+                        >
+                          <BiSolidEdit />
+                        </button>
+                        &nbsp;
+                        <button
+                          onClick={() => deleteHandler(user._id)}
+                          className="primary-button font-bold underline"
+                          type="button"
+                        >
+                          <BsTrash3 />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   );
 }
+
+AdminUsersScreen.auth = { adminOnly: true };
+export default AdminUsersScreen;
