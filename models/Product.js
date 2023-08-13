c0458077e46afade7c21a225a7fd49ef4c7151d2
bygabymora <bygabymora@gmail.com>
2023-08-13 18:23:55 -0500

    Products

--- models/Product.js
+++ models/Product.js
@@ -17,6 +17,7 @@ const productSchema = new mongoose.Schema(
     bulk: { type: String, required: true },
     countInStock: { type: Number, default: 0, required: true },
     countInStockBulk: { type: Number, default: 0, required: true },
+    sentOvernight: { type: Boolean, default: false, required: true },
     notes: { type: String, required: false },
     includes: { type: String, required: true },
   },
