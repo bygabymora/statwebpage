76964a28e3a32e2bc1c37380405cd19f2873d6fc
bygabymora <bygabymora@gmail.com>
2023-08-27 17:36:59 -0500

    User page

--- pages/api/admin/users/[id].js
+++ pages/api/admin/users/[id].js
@@ -1,17 +1,47 @@
-import User from '../../../../models/user';
-import db from '../../../../utils/db';
+import User from '../../../../../models/user';
+import db from '../../../../../utils/db';
 import { getToken } from 'next-auth/jwt';
 
 const handler = async (req, res) => {
   const user = await getToken({ req });
   if (!user || !user.isAdmin) {
-    return res.status(401).send('admin signin required');
+    return res
+      .status(401)
+      .send('Requerido Inicio de sesión como Administrador');
   }
 
-  if (req.method === 'DELETE') {
-    return deleteHandler(req, res);
+  if (req.method === 'GET') {
+    return getHandler(req, res, user);
+  } else if (req.method === 'PUT') {
+    return putHandler(req, res, user);
+  } else if (req.method === 'DELETE') {
+    return deleteHandler(req, res, user);
   } else {
-    return res.status(400).send({ message: 'Method not allowed' });
+    return res.status(400).send({ message: 'Metodo no permitido' });
+  }
+};
+
+const getHandler = async (req, res) => {
+  await db.connect();
+  const user = await User.findById(req.query.id);
+  await db.disconnect();
+  res.send(user);
+};
+
+const putHandler = async (req, res) => {
+  await db.connect();
+  const user = await User.findById(req.query.id);
+  if (user) {
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+    await user.save();
+
+    await db.disconnect();
+    res.send({ message: 'Usuario actualizado exiosamente' });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Usuario no encontrado' });
   }
 };
 
@@ -21,14 +51,16 @@ const deleteHandler = async (req, res) => {
   if (user) {
     if (user.isAdmin === true) {
       await db.disconnect();
-      return res.status(400).send({ message: 'Can not delete admin' });
+      return res
+        .status(400)
+        .send({ message: 'No se puede eliminar al administrador' });
     }
     await User.findByIdAndDelete(req.query.id);
     await db.disconnect();
-    res.send({ message: 'User Deleted' });
+    res.send({ message: 'Usuario borrado' });
   } else {
     await db.disconnect();
-    res.status(404).send({ message: 'User Not Found' });
+    res.status(404).send({ message: 'Usuario no encontrado' });
   }
 };
 
