6436cab0ea20f09e1963ec8add09d8aa9283ee7f
bygabymora <bygabymora@gmail.com>
2023-08-27 17:35:10 -0500

    [id*.js

---
+++ pages/admin/user/[id].js
@@ -0,0 +1,200 @@
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useEffect, useReducer, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import Layout from '../../../components/Layout';
+import { getError } from '../../../utils/error';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    case 'UPDATE_REQUEST':
+      return { ...state, loadingUpdate: true, errorUpdate: '' };
+    case 'UPDATE_SUCCESS':
+      return { ...state, loadingUpdate: false, errorUpdate: '' };
+    case 'UPDATE_FAIL':
+      return { ...state, loadingUpdate: false, errorUpdate: action.payload };
+
+    case 'UPLOAD_REQUEST':
+      return { ...state, loadingUpload: true, errorUpload: '' };
+    case 'UPLOAD_SUCCESS':
+      return {
+        ...state,
+        loadingUpload: false,
+        errorUpload: '',
+      };
+    case 'UPLOAD_FAIL':
+      return { ...state, loadingUpload: false, errorUpload: action.payload };
+
+    default:
+      return state;
+  }
+}
+export default function AdminUserEditScreen() {
+  const [isAdmin, setEsAdmin] = useState(false);
+  const { query } = useRouter();
+  const userId = query.id;
+  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/users/${userId}`);
+        dispatch({ type: 'FETCH_SUCCESS' });
+        setValue('name', data.name);
+        setValue('email', data.email);
+        setValue('isAdmin', data.isAdmin);
+        setEsAdmin(data.isAdmin);
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+
+    fetchData();
+  }, [userId, setValue]);
+
+  const router = useRouter();
+
+  const submitHandler = async ({ name, email, password }) => {
+    try {
+      dispatch({ type: 'UPDATE_REQUEST' });
+      await axios.put(`/api/admin/users/${userId}`, {
+        name,
+        email,
+        password,
+        isAdmin,
+      });
+      dispatch({ type: 'UPDATE_SUCCESS' });
+      toast.success('Usuario actualizado exitosamente');
+      router.push('/admin/users');
+    } catch (err) {
+      dispatch({ type: 'UPDATE_FAIL', payload: getError(err) });
+      toast.error(getError(err));
+    }
+  };
+
+  return (
+    <Layout title={`Editar Usuario ${userId}`}>
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div>
+          <ul>
+            <li>
+              <Link href="/admin/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">Órders</Link>
+            </li>
+            <li>
+              <Link href="/admin/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/admin/users" className="font-bold">
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="md:col-span-3">
+          {loading ? (
+            <div>Cargando...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <form
+              className="mx-auto max-w-screen-md"
+              onSubmit={handleSubmit(submitHandler)}
+            >
+              <h1 className="mb-4 text-xl">{`Editar Usuario ${userId
+                .substring(userId.length - 8)
+                .toUpperCase()}`}</h1>
+              <div className="mb-4">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="name"
+                  autoFocus
+                  {...register('name', {
+                    required: 'Por favor ingrese un nombre',
+                  })}
+                />
+                {errors.name && (
+                  <div className="text-red-500">{errors.name.message}</div>
+                )}
+              </div>
+
+              <div className="mb-4">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="email"
+                  {...register('email', {
+                    required: 'Por favor ingrese el email',
+                  })}
+                />
+                {errors.email && (
+                  <div className="text-red-500">{errors.email.message}</div>
+                )}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="isAdmin">Is Admin?</label>
+                &nbsp;
+                <input
+                  type="checkbox"
+                  id="esAdmin"
+                  {...register('isAdmin')}
+                  checked={isAdmin}
+                  onChange={(e) => {
+                    setValue('isAdmin', e.target.checked);
+                    setEsAdmin(e.target.checked);
+                  }}
+                />
+              </div>
+
+              <div className="flex flex-row">
+                <div className="mb-4">
+                  <button
+                    disabled={loadingUpdate}
+                    className="primary-button mr-2"
+                  >
+                    {loadingUpdate ? 'Cargando' : 'Actualizar'}
+                  </button>
+                </div>
+                <div className="mb-4">
+                  <button
+                    onClick={() => router.push(`/`)}
+                    className="primary-button"
+                  >
+                    Atrás
+                  </button>
+                </div>
+              </div>
+            </form>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+AdminUserEditScreen.auth = { adminOnly: true };
