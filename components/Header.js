14dc57896b2ff2e7e8f3e9a8bdd68c8a3abfad6c
bygabymora <bygabymora@gmail.com>
2023-08-28 10:03:33 -0500

    search

--- components/Header.js
+++ components/Header.js
@@ -18,6 +18,10 @@ const Header = () => {
   const { cart } = state;
   const [cartItemsCount, setCarItemsCount] = useState(0);
   const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  
+
 
   useEffect(() => {
     setCarItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
@@ -32,8 +36,23 @@ const Header = () => {
     }
   };
 
-  const handleSearchInputChange = (e) => {
+  const handleSearchInputChange = async (e) => {
     setSearchQuery(e.target.value);
+    
+    if (e.target.value.length >= 2) {
+      try {
+        const { data } = await axios.get('/api/search', {
+          params: {
+            keyword: e.target.value,
+          },
+        });
+        setSuggestions(data);
+      } catch (error) {
+        console.error("Failed to fetch suggestions:", error);
+      }
+    } else {
+      setSuggestions([]);
+    }
   };
 
   const handleSearch = async () => {
@@ -56,22 +75,50 @@ const Header = () => {
     }
   };
 
+  const handleSuggestionClick = (suggestion) => {
+    setSearchQuery(suggestion);
+    setSuggestions([]); // clear suggestions once one is clicked
+  };
+  
+
+
   return (
     <header className="header">
       <nav className="nav container text-center flex-row-reverse ">
-        <div className="search-field flex col-span-2 text-center ml-4 mr-4">
-          <input
-            value={searchQuery}
-            onChange={handleSearchInputChange}
-            onKeyDown={handleKeyDown}
-            type="text"
-            className="bg-transparent border-b-2 border-blue-900 outline-none focus:bg-white focus:border-blue-900 md:ml-4 w-full"
-            placeholder="Search..."
-          />
-          <button className="nav__search-button" onClick={handleSearch}>
-            <BiSearch className="nav__search-icon" />
-          </button>
+      <div className="search-field relative flex col-span-2 text-center ml-4 mr-4">
+    <div className="flex flex-col items-center w-full">
+        <div className="flex flex-row w-full">
+            <input
+                value={searchQuery}
+                onChange={handleSearchInputChange}
+                onKeyDown={handleKeyDown}
+                type="text"
+                className="bg-transparent border-b-2 border-blue-900 outline-none focus:bg-white focus:border-blue-900 md:ml-4 w-full"
+                placeholder="Search..."
+                onBlur={() => setSuggestions([])}
+            />
+            <button className="nav__search-button" onClick={handleSearch}>
+                <BiSearch className="nav__search-icon" />
+            </button>
         </div>
+
+                      {suggestions.length > 1 && (
+                <div className="suggestions-list absolute top-full w-full mt-1 bg-white shadow-lg z-10">
+                  {suggestions.map((product, idx) => (
+                    <div 
+                    key={idx} 
+                    className="suggestion-item px-3 py-2 hover:bg-gray-200" 
+                    onMouseDown={() => handleSuggestionClick(product.slug)}
+                  >
+                    {product.slug}
+                  </div>
+                  ))}
+                </div>
+              )}
+
+              </div>
+          </div>
+
         <div className="flex h-12 items-center nav-field">
           <div className="flex h-12 items-center">
             <Link href="/" className="nav__logo logo" onClick={handleHomeClick}>
