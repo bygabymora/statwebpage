f2e0068d66c3e10ddfb9992073b60e2452c0e717
bygabymora <bygabymora@gmail.com>
2023-08-29 12:07:36 -0500

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import { SessionProvider, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import CookieAcceptancePopup from '../components/CookieAcceptancePopup';
-import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
@@ -16,7 +15,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           {Component.auth ? (
             <Auth adminOnly={Component.auth.adminOnly}>
               <Component {...pageProps} />
-              <Analytics />
             </Auth>
           ) : (
             <Component {...pageProps} />
