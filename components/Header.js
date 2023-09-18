c6f1cd056eaa35400f345646b5ce145037ec8b98
bygabymora <bygabymora@gmail.com>
2023-09-18 12:11:03 -0500

    search updated

--- components/Header.js
+++ components/Header.js
@@ -20,9 +20,6 @@ const Header = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 
-  
-
-
   useEffect(() => {
     setCarItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
   }, [cart.cartItems]);
@@ -38,7 +35,7 @@ const Header = () => {
 
   const handleSearchInputChange = async (e) => {
     setSearchQuery(e.target.value);
-    
+
     if (e.target.value.length >= 2) {
       try {
         const { data } = await axios.get('/api/search', {
@@ -48,24 +45,26 @@ const Header = () => {
         });
         setSuggestions(data);
       } catch (error) {
-        console.error("Failed to fetch suggestions:", error);
+        console.error('Failed to fetch suggestions:', error);
       }
     } else {
       setSuggestions([]);
     }
   };
 
-  const handleSearch = async () => {
+  const handleSearch = async (query) => {
+    const searchWord = query || searchQuery.trim();
+
     await axios.post('/api/searched', {
-      searchedWord: searchQuery.trim(),
+      searchedWord: searchWord,
       slug: 'raw-search',
       manufacturer: 'raw-search',
       fullName: 'raw-search',
       email: 'raw-search',
     });
 
-    if (searchQuery.trim()) {
-      router.push(`/search?query=${searchQuery}`);
+    if (searchWord) {
+      router.push(`/search?query=${searchWord}`);
     }
   };
 
@@ -78,17 +77,16 @@ const Header = () => {
   const handleSuggestionClick = (suggestion) => {
     setSearchQuery(suggestion);
     setSuggestions([]); // clear suggestions once one is clicked
+    handleSearch(suggestion); // pass the selected suggestion to the search function
   };
-  
-
 
   return (
     <header className="header">
       <nav className="nav container text-center flex-row-reverse ">
-      <div className="search-field relative flex col-span-2 text-center ml-4 mr-4">
-    <div className="flex flex-col items-center w-full">
-        <div className="flex flex-row w-full">
-            <input
+        <div className="search-field relative flex col-span-2 text-center ml-4 mr-4">
+          <div className="flex flex-col items-center w-full">
+            <div className="flex flex-row w-full">
+              <input
                 value={searchQuery}
                 onChange={handleSearchInputChange}
                 onKeyDown={handleKeyDown}
@@ -96,28 +94,27 @@ const Header = () => {
                 className="bg-transparent border-b-2 border-blue-900 outline-none focus:bg-white focus:border-blue-900 md:ml-4 w-full"
                 placeholder="Search..."
                 onBlur={() => setSuggestions([])}
-            />
-            <button className="nav__search-button" onClick={handleSearch}>
+              />
+              <button className="nav__search-button" onClick={handleSearch}>
                 <BiSearch className="nav__search-icon" />
-            </button>
-        </div>
+              </button>
+            </div>
 
-                      {suggestions.length > 1 && (
-                <div className="suggestions-list absolute top-full w-full mt-1 bg-white shadow-lg z-10">
-                  {suggestions.map((product, idx) => (
-                    <div 
-                    key={idx} 
-                    className="suggestion-item px-3 py-2 hover:bg-gray-200" 
+            {suggestions.length > 1 && (
+              <div className="suggestions-list absolute top-full w-full mt-1 bg-white shadow-lg z-10">
+                {suggestions.map((product, idx) => (
+                  <div
+                    key={idx}
+                    className="suggestion-item px-3 py-2 hover:bg-gray-200"
                     onMouseDown={() => handleSuggestionClick(product.slug)}
                   >
                     {product.slug}
                   </div>
-                  ))}
-                </div>
-              )}
-
+                ))}
               </div>
+            )}
           </div>
+        </div>
 
         <div className="flex h-12 items-center nav-field">
           <div className="flex h-12 items-center">
