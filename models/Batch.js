719f1e998c8f832dd65d483fff3d945bd153e2a6
bygabymora <bygabymora@gmail.com>
2023-11-15 14:19:05 -0500

    Batch

---
+++ models/Batch.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const batchSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    slug: { type: String, required: true },
+    lot: { type: String, required: true },
+    expiration: { type: String, required: true },
+    reference: { type: String, required: false, unique: true },
+    countInStock: { type: Number, default: 0, required: true },
+    countInStockBulk: { type: Number, default: 0, required: true },
+    countInStockClearance: { type: Number, default: 0, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Batch = mongoose.models.Batch || mongoose.model('Batch', batchSchema);
+
+export default Batch;
