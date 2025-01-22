78141e9ada9d9227a91553254fe84f769842cbf7
Sofia G <sgommezmm@gmail.com>
2025-01-22 11:08:47 -0500

    CookieAcceptance

--- components/CookieAcceptancePopup.js
+++ components/CookieAcceptancePopup.js
@@ -35,23 +35,24 @@ const CookieAcceptancePopup = () => {
     <div
       className={`${
         isTop ? 'top-0' : 'bottom-0'
-      } fixed left-0 w-full p-4 bg-gray-500 text-white shadow-md z-50`}
+      } fixed left-0 w-full p-6 bg-gray-500 bg-opacity-70 backdrop-blur-md shadow-md text-white z-50`}
     >
       <div className="container mx-auto">
-        <div className="flex justify-between items-center">
-          <p className="text-sm">
+        <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0">
+          <p className="text-sm text-center md:text-left">
             We use cookies to enhance your experience. By continuing to use our
             website, you agree to our cookies use.
+            <a href="/privacy-policy" className="text-white hover:underline"> Learn more</a>
           </p>
           <div className="flex space-x-4">
             <button
-              className="px-4 py-2 bg-green-800 hover:bg-titlecolordark text-white rounded hover:text-black transition duration-300"
+              className="px-5 py-2 bg-green-800 hover:bg-titlecolordark text-white rounded-full hover:shadow-lg hover:text-black transition duration-300 ease-in-out"
               onClick={() => dispatch({ type: 'ACCEPT_COOKIES' })}
             >
               I Understand
             </button>
             <button
-              className="px-4 py-2 bg-red-300 hover:bg-red-100 text-black rounded transition duration-300"
+              className="px-5 py-2 bg-red-300 hover:bg-red-100 text-black rounded-full hover:shadow-lg transition duration-300 ease-in-out"
               onClick={declineCookies}
             >
               Decline
