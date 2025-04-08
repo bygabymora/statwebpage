7fe9f97c7a8dd7674280b4c58d329b1abb2b3c5b
Sofia G <sgommezmm@gmail.com>
2025-04-08 18:46:09 -0500

    Fix OrderScreen: update Stripe initialization to use the correct publishable key

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -19,7 +19,7 @@ import handleSendEmails from '../../utils/alertSystem/documentRelatedEmail';
 import { useModalContext } from '../../components/context/ModalContext';
 
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+  process.env.STRIPE_SECRET_KEY
 );
 
 function reducer(state, action) {
