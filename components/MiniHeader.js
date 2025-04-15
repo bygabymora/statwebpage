af8eb2428faa3828e65bacaf76410410a335b44f
Sofia G <sgommezmm@gmail.com>
2025-04-14 19:31:02 -0500

    fix: add target and rel attributes to social media links for improved accessibility and security

--- components/MiniHeader.js
+++ components/MiniHeader.js
@@ -13,11 +13,18 @@ export default function Header() {
         </div>
 
         <div className="flex space-x-4 items-center">
-          <a href="https://www.facebook.com/statsurgicalsupply" className="hover:text-gray-400"><FaFacebookF /></a>
-          <a href="mailto:sales@statsurgicalsupply.com" className="hover:text-gray-400"><FaEnvelope /></a>
-          <a href="https://www.linkedin.com/company/statsurgicalsupply" className="hover:text-gray-400"><FaLinkedinIn /></a>
+          <a href="https://www.facebook.com/statsurgicalsupply" target="_blank" rel="noopener noreferrer" aria-label="Visit our Facebook page" className="hover:text-gray-400">
+            <FaFacebookF />
+          </a>
+          <a href="mailto:sales@statsurgicalsupply.com" aria-label="Send an email to sales@statsurgicalsupply.com" className="hover:text-gray-400">
+            <FaEnvelope />
+          </a>
+          <a href="https://www.linkedin.com/company/statsurgicalsupply" target="_blank" rel="Visit our LinkedIn profile" aria-label="Visit our Facebook page" className="hover:text-gray-400">
+            <FaLinkedinIn />
+          </a>
           <a href="https://www.google.com/maps/place/100+Ashley+Dr+S+%23600,+Tampa,+FL+33602,+EE.+UU./@27.9446387,-82.4577838,581m/data=!3m2!1e3!4b1!4m6!3m5!1s0x88c2c48c390490ab:0x202198cbac670f1a!8m2!3d27.9446387!4d-82.4577838!16s%2Fg%2F11q_6clqzb?entry=ttu&g_ep=EgoyMDI1MDMzMS4wIKXMDSoJLDEwMjExNDU1SAFQAw%3D%3D" 
-            className="hover:text-gray-400"><FaMapMarkerAlt />
+            target="_blank" rel="noopener noreferrer" aria-label="See our location on Google Maps" className="hover:text-gray-400">
+            <FaMapMarkerAlt />
           </a>
           <span>|</span>
           <a href="tel:+18136074110" className="hover:underline">(813) 607-4110</a>
