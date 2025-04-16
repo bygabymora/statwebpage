ba499e7a06ff5576c2a17463d1f5a00393d36781
Sofia G <sgommezmm@gmail.com>
2025-04-15 19:31:51 -0500

    chore: update project structure and clean up unused files

--- pages/_document.js
+++ pages/_document.js
@@ -4,22 +4,55 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Preload so the font doesn't block rendering */}
+        {/* Preload all fonts */}
         <link
           rel="preload"
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap"
           as="style"
         />
-        {/* Non-blocking load */}
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600&display=swap"
+          as="style"
+        />
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;500;600&display=swap"
+          as="style"
+        />
+
+        {/* Load fonts non-blocking */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap"
           rel="stylesheet"
           media="print"
           onLoad="this.media='all'"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600&display=swap"
+          rel="stylesheet"
+          media="print"
+          onLoad="this.media='all'"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;500;600&display=swap"
+          rel="stylesheet"
+          media="print"
+          onLoad="this.media='all'"
+        />
+
+        {/* Fallback for no-JS */}
         <noscript>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;500;600&display=swap"
             rel="stylesheet"
           />
         </noscript>
