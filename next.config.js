79d395507ffef727d572520be0ccf04c9aaf8979
Sofia G <sgommezmm@gmail.com>
2025-04-15 20:20:29 -0500

    fix: add webpack fallback for buffer in next.config.js

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
       },
     },
   },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      buffer: false,
+    };
+    return config;
+  },
   images: {
     remotePatterns: [
       {
