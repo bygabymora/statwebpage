4a84a3c8e203324c5c0032d6fec65e13ff55d2f5
Sofia G <sgommezmm@gmail.com>
2025-04-24 16:46:14 -0500

    fix: standardize string formatting and improve code readability in orders and order history pages

--- pages/admin/orders.js
+++ pages/admin/orders.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useReducer } from 'react';
-import Layout from '../../components/main/Layout';
-import { getError } from '../../utils/error';
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useReducer } from "react";
+import Layout from "../../components/main/Layout";
+import { getError } from "../../utils/error";
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
-    case 'FETCH_SUCCESS':
-      return { ...state, loading: false, orders: action.payload, error: '' };
-    case 'FETCH_FAIL':
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, orders: action.payload, error: "" };
+    case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
     default:
       state;
@@ -18,111 +18,132 @@ function reducer(state, action) {
 }
 
 export default function AdminOrderScreen() {
-  const [{ loading, error, orders, loadingDelete }, dispatch] = useReducer(reducer, {
-    loading: true,
-    orders: [],
-    error: '',
-  });
+  const [{ loading, error, orders, loadingDelete }, dispatch] = useReducer(
+    reducer,
+    {
+      loading: true,
+      orders: [],
+      error: "",
+    }
+  );
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        dispatch({ type: 'FETCH_REQUEST' });
+        dispatch({ type: "FETCH_REQUEST" });
         const { data } = await axios.get(`/api/admin/orders`);
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchData();
   }, []);
 
   const links = [
-    { href: '/admin/dashboard', label: 'Dashboard'},
-    { href: '/admin/orders', label: 'Orders', isBold: true},
-    { href: '/admin/products', label: 'Products'},
-    { href: '/admin/users', label: 'Users'},
-    { href: '/admin/news', label: 'News'},
+    { href: "/admin/dashboard", label: "Dashboard" },
+    { href: "/admin/orders", label: "Orders", isBold: true },
+    { href: "/admin/products", label: "Products" },
+    { href: "/admin/users", label: "Users" },
+    { href: "/admin/news", label: "News" },
   ];
 
   return (
-    <Layout title="Admin Dashboard">
-        <div className="flex justify-center">
-          <ul className="flex space-x-4 my-3 lg:text-lg w-full">
-            {links.map(({ href, label, isBold}) => (
-              <li key={href} className="w-full">
-                <Link href={href} 
+    <Layout title='Admin Dashboard'>
+      <div className='flex justify-center'>
+        <ul className='flex space-x-4 my-3 lg:text-lg w-full'>
+          {links.map(({ href, label, isBold }) => (
+            <li key={href} className='w-full'>
+              <Link
+                href={href}
                 className={`flex items-center justify-center py-2 bg-white rounded-2xl shadow-md hover:bg-gray-100 transition 
-                  ${isBold ? 'font-semibold' : ''}`} 
-                >
-                  {label}
-                </Link> 
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="md:col-span-3 p-4">
-          <h1 className="text-2xl font-bold mb-4">Admin Orders</h1>
-          {loadingDelete && <div>Deleting...</div>}
-          {loading ? (
-            <div>Loading...</div>
-          ) : error ? (
-            <div className="text-red-500">{error}</div>
-          ) : (
-            <div className="overflow-x-auto">
-              <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-                <thead className="bg-gray-100 border border-collapse">
-                  <tr>
-                    {['ID', 'User', 'Date', 'Total', 'PAID', 'Processed', 'Delivered', 'Actions'].map((header) => (
-                      <th key={header} className="p-4 text-left uppercase border border-collapse">
-                        {header}
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {orders.map((order) => (
-                    <tr key={order._id} className="border-b hover:bg-gray-100">
-                      <td className="border border-collapse p-5">{order?._id ? order._id.substring(20, 24):'No ID'}</td>
-                      <td className="border border-collapse p-5">
-                      {order?.WpUser?.name ? order.WpUser.name : 'DELETED USER'}
-                      </td>
-                      <td className="border border-collapse p-5">
-                      {order?.createdAt ? order.createdAt.substring(0, 10) : 'No Date'}
-                      </td>
-                      <td className="border border-collapse p-5">${order.totalPrice}</td>
-                      <td className="border border-collapse p-5">
-                        {order.isPaid
-                          ? `${order.paidAt.substring(0, 10)}`
-                          : 'not paid'}
-                      </td>
-                      <td className="border border-collapse p-5">
-                        {order.isDelivered
-                          ? `${order.deliveredAt.substring(0, 10)}`
-                          : 'Not Processed'}
-                      </td>
-                      <td className="border border-collapse p-5">
-                        {order.isAtCostumers
-                          ? `${order.atCostumersDate.substring(0, 10)}`
-                          : 'not delivered'}
-                      </td>
-                      <td className="border border-collapse p-5">
-                        <Link
-                          className=" underline font-bold "
-                          href={`/order/${order._id}`}
-                          passHref
-                        >
-                          Details
-                        </Link>
-                      </td>
-                    </tr>
+                  ${isBold ? "font-semibold" : ""}`}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='md:col-span-3 p-4'>
+        <h1 className='text-2xl font-bold mb-4'>Admin Orders</h1>
+        {loadingDelete && <div>Deleting...</div>}
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div className='text-red-500'>{error}</div>
+        ) : (
+          <div className='overflow-x-auto'>
+            <table className='min-w-full bg-white shadow-md rounded-lg overflow-hidden'>
+              <thead className='bg-gray-100 border border-collapse'>
+                <tr>
+                  {[
+                    "ID",
+                    "User",
+                    "Date",
+                    "Total",
+                    "PAID",
+                    "Processed",
+                    "Delivered",
+                    "Actions",
+                  ].map((header) => (
+                    <th
+                      key={header}
+                      className='p-4 text-left uppercase border border-collapse'
+                    >
+                      {header}
+                    </th>
                   ))}
-                </tbody>
-              </table>
-            </div>
-          )}
-        </div>
-      
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((order) => (
+                  <tr key={order._id} className='border-b hover:bg-gray-100'>
+                    <td className='border border-collapse p-5'>
+                      {order?._id ? order._id.substring(20, 24) : "No ID"}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      {order?.WpUser?.name ? order.WpUser.name : "DELETED USER"}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      {order?.createdAt
+                        ? order.createdAt.substring(0, 10)
+                        : "No Date"}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      ${order.totalPrice}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      {order.isPaid
+                        ? `${order.paidAt.substring(0, 10)}`
+                        : "Not Paid"}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      {order.isDelivered
+                        ? `${order.deliveredAt.substring(0, 10)}`
+                        : "Not Processed"}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      {order.isAtCostumers
+                        ? `${order.atCostumersDate.substring(0, 10)}`
+                        : "not delivered"}
+                    </td>
+                    <td className='border border-collapse p-5'>
+                      <Link
+                        className=' underline font-bold '
+                        href={`/order/${order._id}`}
+                        passHref
+                      >
+                        Details
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+      </div>
     </Layout>
   );
 }
--- pages/order-history.js
+++ pages/order-history.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
-import Link from 'next/link';
-import React, { useEffect, useReducer } from 'react';
-import Layout from '../components/main/Layout';
-import { getError } from '../utils/error';
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useReducer } from "react";
+import Layout from "../components/main/Layout";
+import { getError } from "../utils/error";
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
-    case 'FETCH_SUCCESS':
-      return { ...state, loading: false, orders: action.payload, error: '' };
-    case 'FETCH_FAIL':
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, orders: action.payload, error: "" };
+    case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
@@ -20,67 +20,78 @@ function OrderHistoryScreen() {
   const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
     loading: true,
     orders: [],
-    error: '',
+    error: "",
   });
 
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        dispatch({ type: 'FETCH_REQUEST' });
+        dispatch({ type: "FETCH_REQUEST" });
         const { data } = await axios.get(`/api/orders/history`);
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
       }
     };
     fetchOrders();
   }, []);
   return (
-    <Layout title="Order History">
-      <h1 className="mb-4 text-xl">Order History</h1>
+    <Layout title='Order History'>
+      <h1 className='mb-4 text-xl'>Order History</h1>
       {loading ? (
         <div>Loading...</div>
       ) : error ? (
-        <div className="alert-error">{error}</div>
+        <div className='alert-error'>{error}</div>
       ) : (
-        <div className="overflow-x-auto mb-4">
-          <table className="table-auto min-w-full border-collapse border">
-            <thead className="border">
+        <div className='overflow-x-auto mb-4'>
+          <table className='table-auto min-w-full border-collapse border'>
+            <thead className='border'>
               <tr>
-                <th className="px-5 border text-left">ID</th>
-                <th className="p-5 border text-left">DATE</th>
-                <th className="p-5 border text-left">TOTAL</th>
-                <th className="p-5 border text-left">PAID</th>
-                <th className="p-5 border text-left">PROCESSED</th>
-                <th className="p-5 border text-left">DELIVERED</th>
-                <th className="p-5 border text-left">ACTION</th>
+                <th className='px-5 border text-left'>ID</th>
+                <th className='p-5 border text-left'>DATE</th>
+                <th className='p-5 border text-left'>TOTAL</th>
+                <th className='p-5 border text-left'>PAID</th>
+                <th className='p-5 border text-left'>PROCESSED</th>
+                <th className='p-5 border text-left'>DELIVERED</th>
+                <th className='p-5 border text-left'>ACTION</th>
               </tr>
             </thead>
             <tbody>
               {orders.map((order) => (
-                <tr key={order._id} className="border">
-                  <td className=" p-5 border">{order._id.substring(20, 24)}</td>
-                  <td className=" p-5 border">{order.createdAt.substring(0, 10)}</td>
-                  <td className=" p-5 border">${order.totalPrice}</td>
-                  <td className=" p-5 border">
+                <tr key={order._id} className='border'>
+                  <td className=' p-5 border'>
+                    {order._id.substring(20, 24).toUpperCase()}
+                  </td>
+                  <td className=' p-5 border'>
+                    {order.createdAt.substring(0, 10)}
+                  </td>
+                  <td className=' p-5 border'>
+                    $
+                    {new Intl.NumberFormat("en-US", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    }).format(order.totalPrice)}
+                  </td>
+                  <td className=' p-5 border'>
                     {order.isPaid
                       ? `${order.paidAt.substring(0, 10)}`
-                      : 'not paid'}
+                      : "Not Paid"}
                   </td>
-                  <td className=" p-5 border">
+                  <td className=' p-5 border'>
                     {order.isDelivered
                       ? `${order.deliveredAt.substring(0, 10)}`
-                      : 'Not Processed'}
+                      : "Not Processed"}
                   </td>
-                  <td className=" p-5 border">
+                  <td className=' p-5 border'>
                     {order.isAtCostumers
                       ? `${order.atCostumersDate.substring(0, 10)}`
-                      : 'Not Delivered'}
+                      : "Not Delivered"}
                   </td>
-                  <td className=" p-5 ">
+                  <td className=' p-5 '>
                     <Link
-                      className="font-bold underline"
-                      href={`/order/${order._id}`} style={{ color: '#144e8b' }}
+                      className='font-bold underline'
+                      href={`/order/${order._id}`}
+                      style={{ color: "#144e8b" }}
                       passHref
                     >
                       Details
