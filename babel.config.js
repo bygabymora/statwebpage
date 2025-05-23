2bd30942000bc8012451862024adcdc60e2c695e
By Gaby Mora <bygabymora@gmail.com>
2025-05-23 12:49:16 -0500

    refactor: remove babel configuration file as it's no longer needed

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react' // For JSX, if any in your tests or transformed files
-  ]
-};
