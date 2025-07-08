a9c04a2f2a50d4a15b18b0d9f81bbabb4a3a7c56
Sofia G <sgommezmm@gmail.com>
2025-07-08 15:51:19 -0500

    feat: add mobile responsiveness to header component

--- components/main/Header.js
+++ components/main/Header.js
@@ -23,6 +23,12 @@ const Header = () => {
   const [suggestions, setSuggestions] = useState([]);
   const { status, data: session } = useSession();
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    isMobileHandler();
+    window.addEventListener("resize", isMobileHandler);
+  }, []);
 
   const active =
     session?.user?.active &&
@@ -102,6 +108,16 @@ const Header = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [isScrolled]);
 
+  const isMobileHandler = () => {
+    if (typeof window !== "undefined") {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    }
+  };
+
   return (
     <header
       className={`header mx-auto transition-all z-40 duration-300 ${
@@ -154,11 +170,8 @@ const Header = () => {
               src={Logo2}
               alt='logo'
               title='STAT Surgical Supply Logo'
-              width={60}
-              height={60}
-              priority
-              className='object-contain'
-              sizes='(max-width: 768px) 64px, 96px'
+              width={isMobile ? 50 : 100}
+              height={isMobile ? 50 : 100}
             />
           </button>
         </div>
