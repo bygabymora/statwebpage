5d9cf233d2b3005539df53b1847df5c29f25b1df
Sofia G <sgommezmm@gmail.com>
2025-08-26 16:07:36 -0500

    Refactor code structure for improved readability and maintainability

--- components/Banner.js
+++ components/Banner.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Banner1 from "../public/images/assets/banner1.jpg";
 import Banner2 from "../public/images/assets/banner2.svg";
-import Banner3 from "../public/images/assets/banner3.svg";
+import Banner3 from "../public/images/assets/banner3.webp";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { FiPhoneForwarded } from "react-icons/fi";
