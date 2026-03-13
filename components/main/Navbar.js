65660709c80e7d3376eff7bdfd33bab262c6446c
Sofia G <sgommezmm@gmail.com>
2026-03-13 11:32:23 -0500

    feat: enhance GridIcon and CloseIcon components to accept additional props

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -64,7 +64,7 @@ function PackageIcon({ className }) {
   );
 }
 
-function GridIcon({ className }) {
+function GridIcon({ className, ...props }) {
   return (
     <svg
       className={className}
@@ -73,13 +73,14 @@ function GridIcon({ className }) {
       width='1em'
       height='1em'
       aria-hidden='true'
+      {...props}
     >
       <path d='M10 3H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zM9 9H5V5h4v4zm11-6h-6a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-1 6h-4V5h4v4zM10 13H4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1zm-1 6H5v-4h4v4zm11-6h-6a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-6a1 1 0 0 0-1-1zm-1 6h-4v-4h4v4z' />
     </svg>
   );
 }
 
-function CloseIcon({ className }) {
+function CloseIcon({ className, ...props }) {
   return (
     <svg
       className={className}
@@ -88,6 +89,7 @@ function CloseIcon({ className }) {
       width='1em'
       height='1em'
       aria-hidden='true'
+      {...props}
     >
       <path d='M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm4.207 12.793-1.414 1.414L12 13.414l-2.793 2.793-1.414-1.414L10.586 12 7.793 9.207l1.414-1.414L12 10.586l2.793-2.793 1.414 1.414L13.414 12l2.793 2.793z' />
     </svg>
