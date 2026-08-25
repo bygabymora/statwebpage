73efbf9fb6747fba58e838f3989b0c3561f1b29e
Sofia G <sgommezmm@gmail.com>
2026-08-25 14:25:10 -0500

    feat: set DNS servers for database connection

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import dns from "node:dns/promises";
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connection = { isConnected: false };
 let lastActivityTime = null;
